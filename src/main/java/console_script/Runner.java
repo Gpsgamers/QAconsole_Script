d25f7ad661bb30c0cd8bfedb53c70752fae6d76d
Gpsgamers <gamestergp000@gmail.com>
2024-09-28 00:02:31 +0530

    ......

--- src/main/java/console_script/Runner.java
+++ src/main/java/console_script/Runner.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
@@ -20,17 +22,19 @@ import io.cucumber.testng.CucumberOptions;
 )
 public class Runner extends AbstractTestNGCucumberTests {
 	public static WebDriver driver;
-	public static String 
-	SA_username, SA_pass_word,
-	Admin_username,Admin_pass_word,
-	url;
-	@BeforeClass  
-	public static void browserlaunchconfiguration() {
-//		driver = driver("chrome");
-//		Environment("QA");
-		driver = new ChromeDriver();
+	public static String Browser, SA_username, SA_pass_word, Admin_username, Admin_pass_word, url;
+
+	@BeforeClass
+	@Parameters({"browser","environment"})
+	public static void browserlaunchconfiguration(String browser, String environment ) {
+		if(browser.equals("Chrome")) {
+			driver = new ChromeDriver();
+		}else if (browser.equals("Edge")) {
+			driver = new EdgeDriver();
+		}else {
+			driver = null;
+		}
 		driver.manage().window().maximize();
-//		driver.get(url);
 	}
 
 	@AfterClass
@@ -38,33 +42,23 @@ public class Runner extends AbstractTestNGCucumberTests {
 		driver.quit();
 	}
 
-//	public static WebDriver driver(String Browser) {
-//		if (Browser.equals("chrome")) {
-//			return new ChromeDriver();
-//		} else if (Browser.equals("Edge")) {
-//			return new EdgeDriver();
-//		} else {
-//			return null;
-//		}
-//	}
-//
-//	public static void Environment(String Env) {
-//		switch (Env) {
-//		case "QA":
-//			url = "";
-//			break;
-//
-//		case "DEV":
-//			
-//			break;
-//		case "Live":
-//			url = "https://console.mirrorfly.com/";
-//			SA_username = "rahul.s@contus.in";
-//			SA_pass_word = "SuperAdmin!@#$1234";
-//			Admin_username = "rahul.s@contus.in";
-//			Admin_pass_word = "SuperAdmin!@#$1234";
-//			break;
-//		}
-//	}
+	public void Environment(String Env) {
+		switch (Env) {
+		case "QA":
+			url = "";
+			break;
+
+		case "DEV":
+
+			break;
+		case "Live":
+			url = "https://console.mirrorfly.com/";
+			SA_username = "rahul.s@contus.in";
+			SA_pass_word = "SuperAdmin!@#$1234";
+			Admin_username = "rahul.s@contus.in";
+			Admin_pass_word = "SuperAdmin!@#$1234";
+			break;
+		}
+	}
 
 }
