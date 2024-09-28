0661c364abf4d9ac69bb63dca1aae09ca2dff8cc
Gpsgamers <gamestergp000@gmail.com>
2024-09-28 18:07:30 +0530

    ....

--- src/main/java/console_script/Runner.java
+++ src/main/java/console_script/Runner.java
@@ -7,9 +7,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
